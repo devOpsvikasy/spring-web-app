3c58d02ac965c1ad777c0f593c493823375f49ce
devOpsVikas <upscmeralakshya@gmail.com>
2022-09-11 11:20:46 +0530

    Spring web app added

---
+++ src/test/java/com/vikas/apps/firstapp/FirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.apps.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
